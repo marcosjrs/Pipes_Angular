b336e965117a1a694877620bf339972e85b36ef5
marcosjrs <marcosjrs80@gmail.com>
2018-10-29 17:25:04 +0100

    Configuración de locale en el modulo de app, para el pipe "date"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
 import { DomSeguroPipe } from './pipes/dom-seguro.pipe';
+
+import localeEs from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localeEs); //Con esto, el pipe date pasa la fecha a formato español.
 
 @NgModule({
   declarations: [
@@ -14,7 +18,7 @@ import { DomSeguroPipe } from './pipes/dom-seguro.pipe';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ { provide: LOCALE_ID, useValue: 'es' } ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
