b1b6816892e19aec7aff31af8a51a47d4541156d
marcosjrs <marcosjrs80@gmail.com>
2018-10-29 17:04:34 +0100

    Pipes básicos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Pipes';
+  nombre = 'Marcos';
+  miArray = [1,2,3,4,5];
+  enteroConDecimales = 0.123456789;
+  salario = 1234.5;
+  pass = "abc123";
+
+  persona = {
+    nombre: "Marcos",
+    apellidos: "JRS",
+    direccion:{
+      calle:"Rúa Percebe",
+      numero:"13"
+    }
+  }
+
+  promesa = new Promise( (resolve, reject) => {
+      setTimeout(()=>resolve('datos resolved de promesa'),3500 );
+    }
+  );
+  fecha =new Date();
+
+  nombreMalEscrito="Fulanito de tal";
+  urlYoutube = "https://www.youtube.com/embed/";
+  codificar = true;
 }
